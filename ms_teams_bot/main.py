8dd25f012e94e4e439b44e31bde9d1a825f782c0
Neill Killgore <nkillgore@gmail.com>
2023-08-20 00:21:53 -0500

    Add script to main to run

--- ms_teams_bot/main.py
+++ ms_teams_bot/main.py
@@ -1,4 +1,11 @@
 import requests
+import crawler
+import url_getter
 
 BASE_URL = "https://learn.microsoft.com/en-us/microsoftteams/"
 toc = requests.get("https://learn.microsoft.com/en-us/microsoftteams/toc.json").json()
+
+urls = url_getter.get_urls(toc, BASE_URL)
+for url in urls:
+    crawler.crawl(url)
+
