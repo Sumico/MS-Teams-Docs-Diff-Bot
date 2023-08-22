ceab5ae0b4680687e0779fa21d62ad87b4e80f93
Neill Killgore <nkillgore@gmail.com>
2023-08-22 00:09:07 -0500

    fix behavior with labeling file change/add type

--- ms_teams_bot/crawler.py
+++ ms_teams_bot/crawler.py
@@ -4,60 +4,85 @@ from bs4 import BeautifulSoup
 import os
 from datetime import datetime
 
-def crawl(url: str) -> tuple:
+def save_page(url, folder, content):
     """
-    Fetches the content of the provided URL and saves it as an HTML file
-    in a directory structure based on the URL slug and either the current
-    date or the date found within the page content. It returns a status
-    indicating if the content was 'added', 'changed', or 'unchanged', 
-    and the URL if it was changed.
+    Save the content of a webpage to a specified folder.
+    
+    Parameters:
+    - url (str): The URL of the page to be saved.
+    - folder (str): The directory where the content should be saved.
+    - content (str): The content to be saved.
+    """
+    filename = 'index.html'
+  
+    # Ensure the directory exists
+    os.makedirs(folder, exist_ok=True) 
+  
+    # Write the content to the file
+    with open(os.path.join(folder, filename), "w") as f:
+        f.write(content)
 
-    :param url: The URL to fetch and save content from.
-    :return: A tuple containing the status and the URL (if changed).
+
+def crawl(url):
+    """
+    Fetch and save the content of a webpage. Determine if the content is new, unchanged, or changed.
+    
+    Parameters:
+    - url (str): The URL of the page to be fetched.
+    
+    Returns:
+    - tuple: A tuple containing the status ('added', 'unchanged', 'changed') and optionally the URL.
     """
     try:
         response = requests.get(url)
-        response.raise_for_status()  # Ensure we're not processing error responses (like 404s)
+        response.raise_for_status()
     except Exception as e:
         print(f"Error fetching {url} - {e}")
         return ("error",)
 
-    soup = BeautifulSoup(response.text, "html.parser")
-    path = urlsplit(url).path
-    slug = path.split("/")[-1]
-    today = datetime.now().strftime("%Y%m%d")
-
-    # Try to get the date from a <time> element in the page
+    soup = BeautifulSoup(response.text, 'html.parser')
     time_element = soup.find("time")
-    if time_element:
-        date = time_element.get("datetime")[:10]
-    else:
-        date = today
-
-    # Check existing saved dates for the URL
+    date = time_element["datetime"][:10] if time_element else datetime.now().strftime("%Y%m%d")
+    slug = urlsplit(url).path.split("/")[-1]
     slug_dir = f"./data/{slug}"
-    existing_dates = [
-        d for d in os.listdir(slug_dir) if os.path.isdir(os.path.join(slug_dir, d))
-    ] if os.path.exists(slug_dir) else []
 
-    # If there's no existing date or the current date is newer, save the content
-    if not existing_dates or date > max(existing_dates):
-        folder = f"./data/{slug}/{date}"
-        os.makedirs(folder, exist_ok=True)
-        # Save the page content
-        with open(os.path.join(folder, "index.html"), "w", encoding="utf-8") as f:
-            f.write(response.text)
-        return ("added",)
-    elif date == max(existing_dates) and not time_element:  # Same date, but no <time> tag
-        existing_file_path = os.path.join(slug_dir, date, "index.html")
-        with open(existing_file_path, "r", encoding="utf-8") as f:
-            existing_content = f.read()
+    if not os.path.exists(slug_dir):
+        save_page(url, f"./data/{slug}/{date}", response.text)
+        return ("added", url)
 
-        # If the content is different, save it with a modified name
-        if existing_content != response.text:
-            folder = f"./data/{slug}/{date}_{datetime.now().strftime('%H%M%S')}"
-            os.makedirs(folder, exist_ok=True)
-            with open(os.path.join(folder, "index.html"), "w", encoding="utf-8") as f:
-                f.write(response.text)
+    if time_element:
+        # Check if content for this date already exists
+        date_folder = os.path.join(slug_dir, date)
+        if os.path.exists(date_folder):
+            existing_content_file = os.path.join(date_folder, "index.html")
+            
+            with open(existing_content_file, 'r') as f:
+                existing_content = f.read()
+            
+            if existing_content != response.text:
+                save_page(url, f"./data/{slug}/{date}", response.text)
+                return ("changed", url)
+        
+        else:
+            save_page(url, f"./data/{slug}/{date}", response.text)
             return ("changed", url)
+    
+    else:
+        # When there's no time element, compare with the most recent scraped version
+        existing_dates = sorted([d for d in os.listdir(slug_dir) if os.path.isdir(os.path.join(slug_dir, d))])
+        latest_date = existing_dates[-1] if existing_dates else None
+        
+        if latest_date:
+            existing_content_file = os.path.join(slug_dir, latest_date, "index.html")
+            with open(existing_content_file, 'r') as f:
+                existing_content = f.read()
+
+            if existing_content != response.text:
+                save_page(url, f"./data/{slug}/{date}", response.text)
+                return ("changed", url)
+        else:
+            save_page(url, f"./data/{slug}/{date}", response.text)
+            return ("changed", url)
+
+    # Default return
     return ("unchanged",)
